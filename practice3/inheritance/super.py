aad8e8931195ad2009b1b771ab5bd2dc0428a61a
asylzhanakkutanov <a_akkutanov@kbtu.kz>
2026-02-17 22:48:46 +0500

    THis the end

---
+++ practice3/inheritance/super.py
@@ -0,0 +1,33 @@
+#1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+# a super() function that will make the child class inherit all the methods and properties from its parent
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+#By using the super() function, you do not have to use the name of the parent element, it will automatically inherit the methods and properties from its parent
+x = Student("Mike", "Olsen")
+x.printname() 
+
+#2
+#add properties
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+    #In the example below, the year 2019 should be a variable, 
+    # and passed into the Student class when creating student objects. To do so, add another parameter in the __init__() function
+    
+#3
+#Add a year parameter, and pass the correct year when creating objects
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
